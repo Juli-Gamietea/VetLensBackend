78ab798a846eeebdbb174e6a48796596468a17ab
Juli-Gamietea <julian_gamietea@hotmail.com>
2023-07-03 22:21:54 -0300

    starting with entities

--- src/main/java/com/api/vetlens/entity/Dog.java
+++ src/main/java/com/api/vetlens/entity/Dog.java
@@ -1,5 +1,29 @@
 package com.api.vetlens.entity;
 
-public class Dog
-{
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 30)
+    private String name;
+    @Column(name = "dog_breed", length = 30)
+    private String dogBreed;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User owner;
 }
--- src/main/java/com/api/vetlens/entity/User.java
+++ src/main/java/com/api/vetlens/entity/User.java
@@ -1,4 +1,28 @@
 package com.api.vetlens.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    @Column(name = "first_name", length = 30)
+    private String firstName;
+    @Column(name = "last_name", length = 30)
+    private String lastname;
+    @OneToMany(mappedBy = "owner")
+    private List<Dog> dogs;
 }
