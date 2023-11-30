08680a3b19ae8813787116aa9e9844091dcb2c45
Juli-Gamietea <julian_gamietea@hotmail.com>
2023-11-30 19:20:43 -0300

    delete secrets

--- src/main/java/com/api/vetlens/config/S3Config.java
+++ src/main/java/com/api/vetlens/config/S3Config.java
@@ -5,18 +5,17 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${aws.access.key}")
-    private String accessKey;
-    @Value("${aws.access.secret}")
-    private String accessSecret;
-    @Value("${aws.region}")
-    private String region;
+
+    private final String accessKey =  System.getProperty("AWS_SECRET_KEY");
+
+    private final String accessSecret = System.getProperty("AWS_ACCESS_SECRET");
+
+    private final String region = System.getProperty("AWS_REGION");
 
     @Bean
     public AmazonS3 s3Client() {
