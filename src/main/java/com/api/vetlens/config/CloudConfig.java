596ffb508834af3e61d0f9b346d7a9da838360c3
Juli-Gamietea <julian_gamietea@hotmail.com>
2023-12-03 18:25:15 -0300

    fix bug

--- src/main/java/com/api/vetlens/config/CloudConfig.java
+++ src/main/java/com/api/vetlens/config/CloudConfig.java
@@ -5,15 +5,19 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudConfig {
+    @Value("${aws.secret.key}")
+    private String accessKey;
+
+    @Value("${aws.access.secret}")
+    private String accessSecret;
     @Bean
     public AmazonS3 s3Client() {
-        String accessKey = System.getenv("AWS_SECRET_KEY");
-        String accessSecret = System.getenv("AWS_ACCESS_SECRET");
         String region = "sa-east-1";
 
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
